71f370aec0a4bf235ed8d73a6a625d78e1ce91de
davemflick <davemflick@yahoo.com>
2021-10-18 11:09:27 -0400

    Change templates from handlebars to twig for inheritance

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'twig');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+// comment
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
